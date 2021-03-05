afc2720ad29ad915622293f33232e8f3581d7f30
yogtew <reachyogusvi@gmail.com>
2021-03-05 15:23:00 +0800

    Update account key

--- src/components/TransactionHistoryPage/TransactionsHistory.js
+++ src/components/TransactionHistoryPage/TransactionsHistory.js
@@ -4,14 +4,7 @@ import axios from 'axios'
 
 const httpUrl = 'https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view';
 const API_KEY = '895Z21koZEasz7rGcQnPw9Z3BgZUoTln4Lnda9jH';
-const ACCOUNT_KEY = {"phoneNumber": "(+65) 96453923",
-"accountKey": "e049e315-7ec7-483c-b41d-0f37121e2550",
-"lastName": "Keebler",
-"username": "Group3",
-"address": "716 Lynch Inlet",
-"email": "group3@techtrek.com",
-"firstName": "Laurel",
-"nric": "S35005657H"};
+const ACCOUNT_KEY = 'e049e315-7ec7-483c-b41d-0f37121e2550';
 
 const TransactionsHistory = () => {
     const [values, setValues] = useState({});
@@ -21,24 +14,17 @@ const TransactionsHistory = () => {
                 method: 'POST',
                 mode: 'cors',
                 headers: {
-                   'x-api-key': API_KEY,
+                   'x-api-key': API_KEY
                 },
-                body: {
-                    'accountKey': JSON.stringify(ACCOUNT_KEY),
-                } 
-
+                body: JSON.stringify({
+                    'accountKey': ACCOUNT_KEY
+                }) 
             })
             .then(response => response.json())
             .then(data => setValues({...data}))
             .catch(error => console.log(error))
         }
         fetchAssetValue();
-
-        // refresh asset price every 30 seconds
-        const interval = setInterval(() => fetchAssetValue(), 30000)
-        return () => {
-          clearInterval(interval);
-        }
     }, [values,setValues]);
     return (
         <div>
@@ -49,7 +35,7 @@ const TransactionsHistory = () => {
                 Order Type {values.orderType}
             </div>
             <div>
-                Data accurate as of {values.timestamp}
+                Data accurate as of: {values.timestamp}
             </div>
             <div>
                 Asset {values.assetSymbol}
