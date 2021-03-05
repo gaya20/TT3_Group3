52a9d4c1514a60ca58b0e56b710f971ab985c189
thawalk <tharunana@gmail.com>
2021-03-05 12:15:18 +0800

    rendering to screen

--- src/components/CurrentPricing/CurrentPricing.js
+++ src/components/CurrentPricing/CurrentPricing.js
@@ -12,13 +12,26 @@ class CurrentPricing extends Component {
     componenetDidUpdate () {
         axios.post('https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/current')
         .then(response => {
+            console.log('hello')
+            console.log(response)
             this.setState({
+
                 assetSymbol: response.data.assetSymbol,
                 price: response.data.price,
                 timeStamp: response.data.timeStamp
             })
         })
 
+    }
+
+    render (){
+        post(
+            <div>
+                <h1>{this.state.assetSymbol}</h1>
+            </div>
+        )
+
+        return info
     }
 
 }
