b4f8c44807ed6a166187e054237e4aaaf4306480
Jun Ming Ooi <junming1901@hotmail.com>
2021-03-05 14:00:09 +0800

    Persistant Login in Progress

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import store from './store.js'
 
 const Login = () => {
 
-    const [ username, setUsername ] = useState("Type username...");
-    const [ password, setPassword ] = useState("Type password...");  
+    const [ username, setUsername ] = useState(localStorage.username);
+    const [ password, setPassword ] = useState(localStorage.password);
 
     const [ showErrorMsg, setShowErrorMsg ] = useState(false);
     const [ rememberMe, setRememberMe ] = useState(false);
@@ -14,18 +13,29 @@ const Login = () => {
     const TryLogin = (event) => {
         event.preventDefault();
 
+        // Input API code to check if username and password is correct
         if (username == "test" && password == "test")
         {
-            // Input API code to check if username and password is correct
-            store.dispatch({ type: 'Login Success'});
+            store.dispatch({ type: 'Login Success', 
+                             payload: {
+                                 Username: username,
+                                 Password: password 
+                             }});
+
+            // Update
+            localStorage.username = rememberMe ? username : "";
+            localStorage.password = rememberMe ? password : "";
+            
+            document.cookie = "username=" + password + ";";
+            document.cookie = "password=" + password + ";";
         }
         else
         {
             setShowErrorMsg(true);
         }
-
     }
 
+    // Functions to edit state variables
     const EditUsername = (event) => {
         setUsername(event.target.value);
     }
@@ -33,13 +43,17 @@ const Login = () => {
     const EditPassword = (event) => {
         setPassword(event.target.value);
     }
+    
+    const EditRememberMe = (event) => {
+        setRememberMe(event.target.value);
+    }
 
     return (
       <div>
         <form onSubmit={ TryLogin }>
           <div><pre>Username   <input value={ username } onChange={ EditUsername }></input></pre></div>
           <div><pre>Password   <input value={ password } onChange={ EditPassword }></input></pre></div>
-          <input type="checkbox" name="RememberMe" value={ rememberMe }></input>Remember me?
+          <input type="checkbox" value={ rememberMe } onChange={ EditRememberMe }></input>Remember me?
           { showErrorMsg ? <div>Incorrect username or password</div> : <div></div> }
           <p></p>
           <button className="Button" type="submit">Login</button>
