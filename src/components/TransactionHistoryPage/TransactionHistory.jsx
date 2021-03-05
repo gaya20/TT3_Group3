dffcd0ff129a9296dfa5f31d287d61e17461735f
yogtew <reachyogusvi@gmail.com>
2021-03-05 12:41:13 +0800

    Add transaction history table

--- src/components/TransactionHistoryPage/TransactionHistory.jsx
+++ src/components/TransactionHistoryPage/TransactionHistory.jsx
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
 
-function TransactionHistory() {
-    return <h1>Hello this is the transactions page</h1>;
-  }
+class TransactionHistory extends React.Component {
+    render(){
+        return (
+            <div className="row">
+         <div className="col-md-6 col-md-offset-3">
+           <div className="panel panel-info">
+             <div className="panel-heading">
+               <b>Your Transactions History</b>
+             </div>
+               <div className="panel-body">
+                 <table className="table">
+                   <thead>
+                     <tr>
+                       <th># Transaction</th>
+                       <th>Date</th>
+                       <th>Type</th>
+                       <th>Quantity</th>	
+                     </tr>
+                   </thead>
+                   <tbody>
+                       Bought
+                   </tbody>
+                 </table>
+               </div>
+           </div>		
+         </div>
+       </div>	
+        );
+    }
+}
 
 export default TransactionHistory
