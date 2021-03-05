638bebb555747ea4aeb96406cf60b887c67d6218
thawalk <tharunana@gmail.com>
2021-03-05 13:04:12 +0800

    changing to use hooks

--- src/components/CurrentPricing/CurrentPricing.js
+++ src/components/CurrentPricing/CurrentPricing.js
@@ -5,44 +5,39 @@ import axios from 'axios'
 const httpUrl = 'https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/current';
 const API_KEY = '895Z21koZEasz7rGcQnPw9Z3BgZUoTln4Lnda9jH';
 
-class CurrentPricing extends Component {
-
-    state = {
-        assetSymbol: null,
-        price: null,
-        timeStamp: null
-    }
-
-    componentDidMount () {
-        axios.post(httpUrl,
-        {headers: {
-            'Content-Type': 'application/json',
-            "Access-Control-Allow-Origin": "*",
-            'x-api-key': API_KEY
-        }},)
-        .then(response => {
-            console.log('hello');
-            console.log(response);
-            this.setState({
-                assetSymbol: response.data.assetSymbol,
-                price: response.data.price, 
-                timeStamp: response.data.timeStamp
+const CurrentPricing = () => {
+    const [values, setValues] = useState({});
+    useEffect(() => {
+        const fetchAssetValue = () => {
+           fetch(httpUrl, {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                   'x-api-key': API_KEY,
+                }
             })
-        })
-
-    }
-
-    render (){
-        let info = (
+            .then(response => response.json())
+            .then(data => setValues({...data}))
+            .catch(error => console.log(error))
+        }
+        fetchAssetValue();
+        // refresh asset price every minute
+        const interval = setInterval(() => fetchAssetValue(), 60000)
+        return () => {
+          clearInterval(interval);
+        }
+    }, [values,setValues]);
+    return (
+        <div>
             <div>
-                <h1>hello</h1>
-                <h1>{this.state.assetSymbol}</h1>
+                {values.assetSymbol} {values.price}
             </div>
-        );
-
-        return info;
-    }
-
+            <div>
+                Data accurate as of {values.timestamp}
+            </div>
+        </div>
+    );
 }
+
 
 export default CurrentPricing
