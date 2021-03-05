b9203ef4fafeb24bb32c2fe63410518f9e4048a1
Charles Lee <charlesleezhaoyi@gmail.com>
2021-03-05 11:54:27 +0800

    Added Bootstrap and borders to Card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 
 const Card = () => {
     return (
-            <div className="Card" style={{ width: "18rem" }} >
+
+        <div className="Card" style={{ width: "18rem" }} >
+            <div className="border border-primary">
                 <div className="card-body" >
                     <h5 className="card-title">Insert Feature</h5>
                     <p className="card-text">Insert Feature Description</p>
-                    <button className="btn btn-primary" href = "/UserInfo">User Info</button>
+                    <button className="btn btn-primary" href="/UserInfo">User Info</button>
+                    
                 </div>
             </div>
+        </div>
     )
 }
 
