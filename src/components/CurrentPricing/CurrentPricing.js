0d6e560b70c83a50d61a94488fd79f8f5ce870b1
thawalk <tharunana@gmail.com>
2021-03-05 11:44:18 +0800

    initial states

---
+++ src/components/CurrentPricing/CurrentPricing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'Axios'
+
+
+class CurrentPricing extends Component {
+    state = {
+        assetSymbol: null,
+        price: null,
+        timeStamp: null
+    }
+
+}
+
+export default CurrentPricing
