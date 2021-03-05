5200a0bb567f60acb9c58930a810f160ad28b3b7
thawalk <tharunana@gmail.com>
2021-03-05 15:10:21 +0800

    formatted epoch time and date

--- src/components/CurrentPricing/CurrentPricing.js
+++ src/components/CurrentPricing/CurrentPricing.js
@@ -1,22 +1,40 @@
-import React,  { useEffect, useState, Component } from 'react';
 import axios from 'axios'
-
+import './currentpricing.css';
+import { useEffect, useState } from 'react'
 const httpUrl = 'https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/current';
 const API_KEY = '895Z21koZEasz7rGcQnPw9Z3BgZUoTln4Lnda9jH';
 
 const CurrentPricing = () => {
     const [values, setValues] = useState({});
+    const formatTime = (unix_timestamp) => {
+        // Create a new JavaScript Date object based on the timestamp
+        // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+        var date = new Date(unix_timestamp * 1000);
+        // Hours part from the timestamp
+        var hours = date.getHours();
+        // Minutes part from the timestamp
+        var minutes = "0" + date.getMinutes();
+        // Seconds part from the timestamp
+        var seconds = "0" + date.getSeconds();
+
+        // Will display time in 10:30:23 format
+        var formattedTime = date.getDay() + '/' + date.getMonth() + '/' + date.getFullYear() + ' ' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        return formattedTime;
+    }
+
     useEffect(() => {
         const fetchAssetValue = () => {
-           fetch(httpUrl, {
+            fetch(httpUrl, {
                 method: 'POST',
                 mode: 'cors',
                 headers: {
-                   'x-api-key': API_KEY,
+                'x-api-key': API_KEY,
                 }
             })
             .then(response => response.json())
-            .then(data => setValues({...data}))
+            .then(data => {
+                    setValues({...data});
+            })
             .catch(error => console.log(error))
         }
         fetchAssetValue();
@@ -25,18 +43,25 @@ const CurrentPricing = () => {
         return () => {
           clearInterval(interval);
         }
-    }, [values,setValues]);
+    }, []);
     return (
-        <div>
-            <div>
-                {values.assetSymbol} {values.price}
-            </div>
-            <div>
-                Data accurate as of {values.timestamp}
+        <div className='currPricing'>
+            <p className='label'>Current Pricing:</p>
+            <div className='card'>
+                <div className='pricing'>
+                    <div className='symbol'>
+                        {values.assetSymbol}
+                    </div>
+                    <div className='price'>
+                        {values.price}
+                    </div>
+                </div>
+                <div className='timestamp'>
+                    Data accurate as of {formatTime(values.timestamp)}
+                </div>
             </div>
         </div>
     );
 }
-
 
 export default CurrentPricing
